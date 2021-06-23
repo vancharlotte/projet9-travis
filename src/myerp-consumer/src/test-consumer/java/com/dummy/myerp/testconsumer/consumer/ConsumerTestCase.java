f99feaf5c1febd5e5678b6fa592a7399e65ff124
charlotte <vanhuyse.charlotte@gmail.com>
2021-06-23 21:02:43 +0200

    add consumer IT

---
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/ConsumerTestCase.java
@@ -0,0 +1,29 @@
+package com.dummy.myerp.testconsumer.consumer;
+
+import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
+
+public abstract class ConsumerTestCase {
+
+
+    static {
+        SpringRegistry.init();
+    }
+
+    /** {@link DaoProxy} */
+    private static final DaoProxy DAO_PROXY = SpringRegistry.getDaoProxy();
+
+
+    // ==================== Constructeurs ====================
+    /**
+     * Constructeur.
+     */
+    public ConsumerTestCase() {
+    }
+
+
+    // ==================== Getters/Setters ====================
+    public static DaoProxy getDaoProxy() {
+        return DAO_PROXY;
+    }
+}
+
