79f492364681168b25c9fc8306bc0013f3f395e1
charlotte <vanhuyse.charlotte@gmail.com>
2021-06-24 06:45:15 +0200

    update consumer IT

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/ComptabiliteDaoImplIT.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/ComptabiliteDaoImplIT.java
@@ -4,6 +4,7 @@ import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
 import com.dummy.myerp.technical.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ComptabiliteDaoImplIT {
 
@@ -55,22 +55,56 @@ public class ComptabiliteDaoImplIT {
 
     @Test
     public void getEcritureComptableByRefTest() throws NotFoundException {
-        EcritureComptable newEC = new EcritureComptable();
-        newEC = dao.getEcritureComptableByRef("AC-2016/00001");
+        EcritureComptable newEC = dao.getEcritureComptableByRef("AC-2016/00001");
         assertEquals(-1,newEC.getId());
-
     }
 
+    @Test
+    public void getEcritureComptableByRefTest_Notfound() throws NotFoundException {
+        EcritureComptable newEC = dao.getEcritureComptableByRef("AC-2021/00001");
+        Exception exception = assertThrows(FunctionalException.class, () -> dao.updateEcritureComptable(newEC));
+        logger.info(exception.getMessage());
+    }
 
     @Test
     public void insertEcritureComptable(){
+        List<EcritureComptable> listEC =  dao.getListEcritureComptable();
+        EcritureComptable newEC = new EcritureComptable();
+        newEC.setId(1);
+        newEC.setReference("BB-2021/00001");
+        newEC.setJournal(new JournalComptable("BB","Test"));
+        dao.insertEcritureComptable(newEC);
+        assertEquals((listEC.size()+1),dao.getListEcritureComptable().size());
+        dao.deleteEcritureComptable(1);
     }
 
     @Test
-    public void updateEcritureComptable(){}
+    public void updateEcritureComptable() throws NotFoundException {
+        EcritureComptable newEC = dao.getEcritureComptable(-1);
+        String oldLibelle = newEC.getLibelle();
+        newEC.setLibelle("TestUpdateEC");
+        dao.updateEcritureComptable(newEC);
+        assertEquals("TestUpdateEC",dao.getEcritureComptable(-1).getLibelle());
+
+        newEC.setLibelle(oldLibelle);
+        dao.updateEcritureComptable(newEC);
+        assertEquals(oldLibelle,dao.getEcritureComptable(-1).getLibelle());
+
+
+    }
 
     @Test
-    public void deleteEcritureComptable(){}
+    public void deleteEcritureComptable(){
+        EcritureComptable newEC = new EcritureComptable();
+        newEC.setId(1);
+        newEC.setReference("BB-2021/00001");
+        newEC.setJournal(new JournalComptable("BB","Test"));
+        dao.insertEcritureComptable(newEC);
+        List<EcritureComptable> listEC =  dao.getListEcritureComptable();
+        dao.deleteEcritureComptable(1);
+        assertEquals((listEC.size()-1),dao.getListEcritureComptable().size());
+
+    }
 
     @Test
     public void loadListLigneEcritureTest(){}
